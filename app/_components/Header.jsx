e9c4a0d569b1dab621984e3bcf66493984bb2241
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-07-03 23:11:35 +0530

    Header

---
+++ app/_components/Header.jsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="p-5 shadow-sm flex justify-between">
+        <div className="flex items-center gap-8">
+        <Image src='/1.png' alt='logo' width={180} height={70} />
+        <div className="md:flex items-center gap-6 font-semibold hidden ">
+            <h2 className="hover:scale-105 hover:text-primary cursor-pointer  ">Home</h2>
+            <h2 className="hover:scale-105 hover:text-primary cursor-pointer  ">Services</h2>
+            <h2 className="hover:scale-105 hover:text-primary cursor-pointer  ">About Us</h2>
+        </div>
+        
+        </div>
+        <div className="mt-10">
+            <Button>Get Started</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
-     <Button>Hello</Button>
+  
     </div>
   );
 }
