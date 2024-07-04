d590ea643469fa65c9d7593b7f5a1ea5f0628de9
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-07-04 21:42:29 +0530

    authentication part

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,10 +1,26 @@
 "use client"
 import { Button } from '@/components/ui/button'
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 
 function Header() {
+
+  const {data}= useSession();
+
+  useEffect(()=>{
+   console.log(data);
+  },[data])
   return (
     <div className="p-5 shadow-sm flex justify-between">
         <div className="flex items-center gap-8">
@@ -17,7 +33,28 @@ function Header() {
         
         </div>
         <div className="mt-4">
-            <Button onClick={()=>signIn('descope')} >Login / SignUp</Button>
+          {data?.user?
+          
+           <DropdownMenu>
+           <DropdownMenuTrigger asChild >
+           <Image src={data?.user?.image} alt='user' 
+          className='rounded-full' width={40} height={40}
+           />
+           </DropdownMenuTrigger>
+           <DropdownMenuContent>
+             <DropdownMenuLabel>My Account</DropdownMenuLabel>
+             <DropdownMenuSeparator />
+             <DropdownMenuItem>My Bookings</DropdownMenuItem>
+             <DropdownMenuItem onClick={()=>signOut()} >Log Out</DropdownMenuItem>
+             
+           </DropdownMenuContent>
+         </DropdownMenu>
+         
+          :
+          
+          <Button onClick={()=>signIn('descope')} >Login / SignUp</Button>
+        }
+            
         </div>
     </div>
   )
